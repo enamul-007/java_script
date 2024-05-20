5e2733c98b0f838b231d96a379ccaf3c10c4bbf5
enamul-007 <anamulkhan523@gmail.com>
2024-05-21 01:57:44 +0600

    main

---
+++ js/main.js
@@ -0,0 +1,72 @@
+var numberOne = 10;
+var numberTow = 20;
+var numberThree = 30;
+var numberFour = 40;
+var numberFive = 50;
+var numberSix = 60;
+var numberSeven = 70;
+var numberEight = 80;
+var numberNine = 90;
+var numberTen = 100;
+
+// sum
+var sum =
+    numberOne +
+    numberTow +
+    numberThree +
+    numberFour +
+    numberFive +
+    numberSix +
+    numberSeven +
+    numberEight +
+    numberNine +
+    numberTen
+
+// susubstraction
+var susubstraction =
+    numberOne -
+    numberTow -
+    numberThree -
+    numberFour -
+    numberFive -
+    numberSix -
+    numberSeven -
+    numberEight -
+    numberNine -
+    numberTen
+
+// multiplication
+
+var multiplication =
+    numberOne *
+    numberTow *
+    numberThree *
+    numberFour *
+    numberFive *
+    numberSix *
+    numberSeven *
+    numberEight *
+    numberNine *
+    numberTen
+
+    // division
+var division =
+    numberOne /
+    numberTow /
+    numberThree /
+    numberFour /
+    numberFive /
+    numberSix /
+    numberSeven /
+    numberEight /
+    numberNine /
+    numberTen
+
+
+
+
+
+console.log("The sum is", sum)
+console.log("The substraction is", susubstraction)
+console.log("The multiplication is", multiplication)
+console.log("The division is", division)
