061675340ec2a44b806115492f971efeaf5e0323
enamul-007 <anamulkhan523@gmail.com>
2024-05-28 20:05:29 +0600

    for loop

--- js/main.js
+++ js/main.js
@@ -1,300 +1,314 @@
-// var numberOne = 10;
-// var numberTow = 20;
-// var numberThree = 30;
-// var numberFour = 40;
-// var numberFive = 50;
-// var numberSix = 60;
-// var numberSeven = 70;
-// var numberEight = 80;
-// var numberNine = 90;
-// var numberTen = 100;
+// // var numberOne = 10;
+// // var numberTow = 20;
+// // var numberThree = 30;
+// // var numberFour = 40;
+// // var numberFive = 50;
+// // var numberSix = 60;
+// // var numberSeven = 70;
+// // var numberEight = 80;
+// // var numberNine = 90;
+// // var numberTen = 100;
 
-// // sum
-// var sum =
-//     numberOne +
-//     numberTow +
-//     numberThree +
-//     numberFour +
-//     numberFive +
-//     numberSix +
-//     numberSeven +
-//     numberEight +
-//     numberNine +
-//     numberTen
+// // // sum
+// // var sum =
+// //     numberOne +
+// //     numberTow +
+// //     numberThree +
+// //     numberFour +
+// //     numberFive +
+// //     numberSix +
+// //     numberSeven +
+// //     numberEight +
+// //     numberNine +
+// //     numberTen
 
-// // susubstraction
-// var susubstraction =
-//     numberOne -
-//     numberTow -
-//     numberThree -
-//     numberFour -
-//     numberFive -
-//     numberSix -
-//     numberSeven -
-//     numberEight -
-//     numberNine -
-//     numberTen
+// // // susubstraction
+// // var susubstraction =
+// //     numberOne -
+// //     numberTow -
+// //     numberThree -
+// //     numberFour -
+// //     numberFive -
+// //     numberSix -
+// //     numberSeven -
+// //     numberEight -
+// //     numberNine -
+// //     numberTen
 
-// // multiplication
+// // // multiplication
 
-// var multiplication =
-//     numberOne *
-//     numberTow *
-//     numberThree *
-//     numberFour *
-//     numberFive *
-//     numberSix *
-//     numberSeven *
-//     numberEight *
-//     numberNine *
-//     numberTen
+// // var multiplication =
+// //     numberOne *
+// //     numberTow *
+// //     numberThree *
+// //     numberFour *
+// //     numberFive *
+// //     numberSix *
+// //     numberSeven *
+// //     numberEight *
+// //     numberNine *
+// //     numberTen
 
-//     // division
-// var division =
-//     numberOne /
-//     numberTow /
-//     numberThree /
-//     numberFour /
-//     numberFive /
-//     numberSix /
-//     numberSeven /
-//     numberEight /
-//     numberNine /
-//     numberTen
-
-
-
-
-
-// console.log("The sum is", sum)
-// console.log("The substraction is", susubstraction)
-// console.log("The multiplication is", multiplication)
-// console.log("The division is", division)
+// //     // division
+// // var division =
+// //     numberOne /
+// //     numberTow /
+// //     numberThree /
+// //     numberFour /
+// //     numberFive /
+// //     numberSix /
+// //     numberSeven /
+// //     numberEight /
+// //     numberNine /
+// //     numberTen
 
 
 
 
 
-
-
-// // ================================================================
-
-// // DATA TYPE //
-
-// let Name = "Enamul houque";
-
-// let Id = 00713;
-
-// let Department = "web & softoware";
-
-// let batch = "mern-2307";
-
-// let mentor = `taufik islam`;
-
-// let reguler = true;
-
-// let TotalePresntClass = 0.99;
-
-//  let versity = null;
-
-
-// console.log(typeof Name)
-
-// console.log(typeof Id)
-
-// console.log(typeof Department)
-
-// console.log(typeof batch)
-
-// console.log(typeof mentor)
-
-// console.log(typeof reguler)
-
-// console.log(typeof TotalePresntClass)
-
-// console.log(typeof versity)
-
-
-// // OPARETOR
-
-// let number1 = 20;
-// let number2 = 30;
-// let number3 = 33;
-// let number4 = 40;
-// number1 += 330;
-// number2 -= 200
-// number3 *= 100
-// number4 /= 10
-
-// console.log("our Increment value is :", number1);
-// console.log("our Decrement value is :", number2);
-// console.log("our Exponentiation value is :", number3);
-// console.log("our Division value is :", number4);
+// // console.log("The sum is", sum)
+// // console.log("The substraction is", susubstraction)
+// // console.log("The multiplication is", multiplication)
+// // console.log("The division is", division)
 
 
 
 
-// // class 31 //
-
-// const userInput = prompt("please input the number");
-// const covertedInput = +userInput;
-
-// if (covertedInput > 60) {
-//     console.log("apni buro hoye gacen akhon rest koren");
-// } else if (covertedInput >= 60) {
-//     console.log("ekhon apnar biye korar khomota nai ");
-// } else if (covertedInput < 18 && covertedInput > 0) {
-//     console.log("tomar matro biyer boyosh hoice");
-// } else if (covertedInput > 19) {
-//     console.log("tomar parfact biyer boyos");
-// }
-// else {
-//     console.log("your are giving worng input");
-// }
-
-// // even or odd //
-
-// const userInput = prompt("please input the number");
-// const covertedInput = +userInput;
-
-// if (covertedInput % 2 == 0 && covertedInput > 0) {
-//     console.log("this is even number");
-// } else if (covertedInput % 2 != 0 && covertedInput > 0) {
-//     console.log("this is odd number");
-// }
 
 
 
-// // switch statment //
+// // // ================================================================
+
+// // // DATA TYPE //
+
+// // let Name = "Enamul houque";
+
+// // let Id = 00713;
+
+// // let Department = "web & softoware";
+
+// // let batch = "mern-2307";
+
+// // let mentor = `taufik islam`;
+
+// // let reguler = true;
+
+// // let TotalePresntClass = 0.99;
+
+// //  let versity = null;
 
 
-// const pressBtn = prompt("press the button");
+// // console.log(typeof Name)
+
+// // console.log(typeof Id)
+
+// // console.log(typeof Department)
+
+// // console.log(typeof batch)
+
+// // console.log(typeof mentor)
+
+// // console.log(typeof reguler)
+
+// // console.log(typeof TotalePresntClass)
+
+// // console.log(typeof versity)
+
+
+// // // OPARETOR
+
+// // let number1 = 20;
+// // let number2 = 30;
+// // let number3 = 33;
+// // let number4 = 40;
+// // number1 += 330;
+// // number2 -= 200
+// // number3 *= 100
+// // number4 /= 10
+
+// // console.log("our Increment value is :", number1);
+// // console.log("our Decrement value is :", number2);
+// // console.log("our Exponentiation value is :", number3);
+// // console.log("our Division value is :", number4);
+
+
+
+
+// // // class 31 //
+
+// // const userInput = prompt("please input the number");
+// // const covertedInput = +userInput;
+
+// // if (covertedInput > 60) {
+// //     console.log("apni buro hoye gacen akhon rest koren");
+// // } else if (covertedInput >= 60) {
+// //     console.log("ekhon apnar biye korar khomota nai ");
+// // } else if (covertedInput < 18 && covertedInput > 0) {
+// //     console.log("tomar matro biyer boyosh hoice");
+// // } else if (covertedInput > 19) {
+// //     console.log("tomar parfact biyer boyos");
+// // }
+// // else {
+// //     console.log("your are giving worng input");
+// // }
+
+// // // even or odd //
+
+// // const userInput = prompt("please input the number");
+// // const covertedInput = +userInput;
+
+// // if (covertedInput % 2 == 0 && covertedInput > 0) {
+// //     console.log("this is even number");
+// // } else if (covertedInput % 2 != 0 && covertedInput > 0) {
+// //     console.log("this is odd number");
+// // }
+
+
+
+// // // switch statment //
+
+
+// // const pressBtn = prompt("press the button");
  
-// switch (pressBtn) {
-//     case "tv":
-//         console.log("tv is on");
-//         break;
-//     case "fan":
-//         console.log("fan is one");
-//         break;
-//     case "ac":
-//         console.log("ac is on");
-//         break;
-//     case "light":
-//         console.log("light is one");
-//         break;
-//     case "door":
-//         console.log("door is open now");
-//         break;
-//     case "door close":
-//         console.log("door close now");
-//         break;
-//     default:
-//         console.log("not match");
-//         break;
+// // switch (pressBtn) {
+// //     case "tv":
+// //         console.log("tv is on");
+// //         break;
+// //     case "fan":
+// //         console.log("fan is one");
+// //         break;
+// //     case "ac":
+// //         console.log("ac is on");
+// //         break;
+// //     case "light":
+// //         console.log("light is one");
+// //         break;
+// //     case "door":
+// //         console.log("door is open now");
+// //         break;
+// //     case "door close":
+// //         console.log("door close now");
+// //         break;
+// //     default:
+// //         console.log("not match");
+// //         break;
+// // }
+
+// //
+
+
+
+// const mayWeek = prompt("type date");
+// switch (+mayWeek) {
+//   case 1:
+//     console.log("today is wednesday");
+//     break;
+//   case 2:
+//     console.log("today is thursday");
+//     break;
+//   case 3:
+//     console.log("today is friday");
+//     break;
+//   case 4:
+//     console.log("today is saturday");
+//     break;
+//   case 5:
+//     console.log("today is sunday");
+//     break;
+//   case 6:
+//     console.log("today is monday");
+//     break;
+//   case 7:
+//     console.log("today is tuesday");
+//     break;
+//   case 8:
+//     console.log("today is wednesday");
+//     break;
+//   case 9:
+//     console.log("today is thursday");
+//     break;
+//   case 10:
+//     console.log("today is friday");
+//     break;
+//   case 11:
+//     console.log("today is saturday");
+//     break;
+//   case 12:
+//     console.log("today is sunday");
+//     break;
+//   case 13:
+//     console.log("today is monday");
+//     break;
+//   case 14:
+//     console.log("today is tuesday");
+//     break;
+//   case 15:
+//     console.log("today is wednesday");
+//     break;
+//   case 16:
+//     console.log("today is thursday");
+//     break;
+//   case 17:
+//     console.log("today is friday");
+//     break;
+//   case 18:
+//     console.log("today is saturday");
+//     break;
+//   case 19:
+//     console.log("today is sunday");
+//     break;
+//   case 20:
+//     console.log("today is monday");
+//     break;
+//   case 21:
+//     console.log("today is tuesday");
+//     break;
+//   case 22:
+//     console.log("today is wednesday");
+//     break;
+//   case 23:
+//     console.log("today is thursday");
+//     break;
+//   case 24:
+//     console.log("today is friday");
+//     break;
+//   case 25:
+//     console.log("today is saturday");
+//     break;
+//   case 26:
+//     console.log("today is sunday");
+//     break;
+//   case 27:
+//     console.log("today is monday");
+//     break;
+//   case 28:
+//     console.log("today is tuesday");
+//     break;
+//   case 29:
+//     console.log("today is wednesday");
+//     break;
+//   case 30:
+//     console.log("today is thursday");
+//     break;
+//   case 31:
+//     console.log("today is friday");
+//     break;
+
+//   default:
+//     console.log("please type date");
+//     break;
 // }
 
-//
 
 
+// loop //
 
-const mayWeek = prompt("type date");
-switch (+mayWeek) {
-  case 1:
-    console.log("today is wednesday");
-    break;
-  case 2:
-    console.log("today is thursday");
-    break;
-  case 3:
-    console.log("today is friday");
-    break;
-  case 4:
-    console.log("today is saturday");
-    break;
-  case 5:
-    console.log("today is sunday");
-    break;
-  case 6:
-    console.log("today is monday");
-    break;
-  case 7:
-    console.log("today is tuesday");
-    break;
-  case 8:
-    console.log("today is wednesday");
-    break;
-  case 9:
-    console.log("today is thursday");
-    break;
-  case 10:
-    console.log("today is friday");
-    break;
-  case 11:
-    console.log("today is saturday");
-    break;
-  case 12:
-    console.log("today is sunday");
-    break;
-  case 13:
-    console.log("today is monday");
-    break;
-  case 14:
-    console.log("today is tuesday");
-    break;
-  case 15:
-    console.log("today is wednesday");
-    break;
-  case 16:
-    console.log("today is thursday");
-    break;
-  case 17:
-    console.log("today is friday");
-    break;
-  case 18:
-    console.log("today is saturday");
-    break;
-  case 19:
-    console.log("today is sunday");
-    break;
-  case 20:
-    console.log("today is monday");
-    break;
-  case 21:
-    console.log("today is tuesday");
-    break;
-  case 22:
-    console.log("today is wednesday");
-    break;
-  case 23:
-    console.log("today is thursday");
-    break;
-  case 24:
-    console.log("today is friday");
-    break;
-  case 25:
-    console.log("today is saturday");
-    break;
-  case 26:
-    console.log("today is sunday");
-    break;
-  case 27:
-    console.log("today is monday");
-    break;
-  case 28:
-    console.log("today is tuesday");
-    break;
-  case 29:
-    console.log("today is wednesday");
-    break;
-  case 30:
-    console.log("today is thursday");
-    break;
-  case 31:
-    console.log("today is friday");
-    break;
+// for loop //
 
-  default:
-    console.log("please type date");
-    break;
-}
+let namta = prompt("Enter your value");
+  let covertNamta = +namta;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(`${covertNamta} x ${i} = ${covertNamta * i}`);
+    
+  }
